fa611d5d70c5256f522bd8917c42e11c96a21078
lozovoyDanil <danilozovoy@gmail.com>
2025-08-31 16:48:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(self, exc_type: type, exc_value: Exception, traceback: str) -> None:
+        try:
+            os.remove(self.filename)
+        except FileNotFoundError:
+            pass
